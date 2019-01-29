a25a5ad4e78f576d6465c5ee4eaaa656a8cdbf2f
Boriss Mejias <boriss.mejias@2ndquadrant.com>
2019-01-29 15:28:59 +0000

    Add count of documents by extension

--- sneak.sql
+++ sneak.sql
@@ -29,6 +29,20 @@ AS $$
 $$;
 
 
+CREATE OR REPLACE FUNCTION alfsneak.docs_by_extension(extension VARCHAR(32))
+RETURNS BIGINT
+LANGUAGE SQL
+AS $$
+    SELECT COUNT(*) as docs
+    FROM alf_node AS n
+    JOIN alf_qname AS q ON (n.type_qname_id = q.id)
+    JOIN alf_node_properties AS p ON (n.id = p.node_id)
+    WHERE p.qname_id IN (SELECT id FROM alf_qname WHERE local_name='name')
+      AND q.local_name='content'
+      AND p.string_value LIKE '%.' || extension;
+$$
+
+
 -- CREATE OR REPLACE FUNCTION alfsneak. 
 -- RETURNS 
 -- LANGUAGE SQL
--- tests.sql
+++ tests.sql
@@ -1,5 +1,9 @@
+-- Number of ...
 SELECT alfsneak.nodes_by_qname('user');
 SELECT alfsneak.n_users();
 SELECT alfsneak.nodes_by_qname('content');
 SELECT alfsneak.n_all_docs();
 
+-- Number of docs
+SELECT alfsneak.docs_by_extension('xml');
+SELECT alfsneak.docs_by_extension('pdf');
